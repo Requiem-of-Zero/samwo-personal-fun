9525a93fa233b8a3e4fdc20cec4050debf0a0dcb
requiem-of-zero <theesamwong@gmail.com>
2025-12-25 18:05:32 -0800

    Added list filter to filter entries by publish date

--- backend_gym/django_backend_tutorial/djangotutorial/polls/admin.py
+++ backend_gym/django_backend_tutorial/djangotutorial/polls/admin.py
@@ -14,6 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
     #     ("Date information", {"fields": ["pub_date"]}),
     # ]
     list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)

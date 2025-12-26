878162bae0d72ad8ae97df22ab59715f4da56338
requiem-of-zero <theesamwong@gmail.com>
2025-12-25 18:00:29 -0800

    Update questions to have a list display from the question, publish date, and pusblied_recently function

--- backend_gym/django_backend_tutorial/djangotutorial/polls/admin.py
+++ backend_gym/django_backend_tutorial/djangotutorial/polls/admin.py
@@ -5,14 +5,15 @@ from .models import Question, Choice
 # admin.site.register(Question)
 # admin.site.register(Choice)
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
-    ]
+    # fieldsets = [
+    #     (None, {"fields": ["question_text"]}),
+    #     ("Date information", {"fields": ["pub_date"]}),
+    # ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)

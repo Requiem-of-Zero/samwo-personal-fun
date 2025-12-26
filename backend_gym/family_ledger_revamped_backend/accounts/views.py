bc07fd3c4fb68345294413adb11baabb22312767
requiem-of-zero <theesamwong@gmail.com>
2025-12-25 22:00:13 -0800

    Added basic register and me views for the register and me serializers

--- backend_gym/family_ledger_revamped_backend/accounts/views.py
+++ backend_gym/family_ledger_revamped_backend/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework import response
 
+from .serializers import RegisterSerializer, MeSerializer
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    """
+    POST /api/v1/auth/register/
+    """
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+class MeView(generics.CreateAPIView):
+    """
+    POST /api/v1/auth/me/
+    """
+    serializer_class = MeSerializer
+
+    def get_current_user_obj(self):
+        return self.request.user # Returns current authenticated user || null

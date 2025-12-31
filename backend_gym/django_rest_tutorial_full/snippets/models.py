3a50e5ea361bcfeb73e06c3d12a0f3df781e59ca
requiem-of-zero <theesamwong@gmail.com>
2025-12-31 12:02:34 -0800

    Updated snippet model with custom save function and added inclusion for a User relationship as the snippet owner

--- backend_gym/django_rest_tutorial_full/snippets/models.py
+++ backend_gym/django_rest_tutorial_full/snippets/models.py
@@ -1,6 +1,8 @@
 from django.db import models # Models module for databases
-from pygments.lexers import get_all_lexers # Module for obtaining the lexers from each variable sent to it
+from pygments.lexers import get_all_lexers, get_lexer_by_name # Module for obtaining the lexers from each variable sent to it and lexer name returns the lexer for a specific language
 from pygments.styles import get_all_styles # Module for obtaining styles for rendered code
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 # Create your models here.
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -13,7 +15,20 @@ class Snippet(models.Model):
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default="python", max_length=100)
-    style=models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
-
+    style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
+    owner = models.ForeignKey("auth.User", related_name="snippets", on_delete=models.CASCADE)
+    highlighted = models.TextField()
+    
     class Meta:
         ordering = ["created"]
+
+    def save(self, *args, **kwargs):
+        """
+        Use the pygments library to create a highlighted HTML representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)

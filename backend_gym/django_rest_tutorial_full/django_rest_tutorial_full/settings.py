3d69f884ddac92b3827b6fa62913e7bc1f026467
requiem-of-zero <theesamwong@gmail.com>
2025-12-31 15:19:51 -0800

    Added basic pagination rest framework settings

--- backend_gym/django_rest_tutorial_full/django_rest_tutorial_full/settings.py
+++ backend_gym/django_rest_tutorial_full/django_rest_tutorial_full/settings.py
@@ -52,6 +52,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
+
 ROOT_URLCONF = 'django_rest_tutorial_full.urls'
 
 TEMPLATES = [

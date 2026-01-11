7e568474b5284045f8733db4b09ce7e341f27631
requiem-of-zero <theesamwong@gmail.com>
2026-01-11 14:36:36 -0800

    Added method to protect routes if not authenticated for transactions

--- all_in_ledger/all_in_ledger_web/app/transactions/page.tsx
+++ all_in_ledger/all_in_ledger_web/app/transactions/page.tsx
@@ -1,5 +1,21 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { getCurrentUserFromRequest } from "@/src/server/auth/currentUser";
 import TransactionsClient from "./TransactionsClient";
 
 export default async function TransactionsPage() {
+  const cookieStore = await cookies();
+  const cookieString = cookieStore.toString();
+
+  const req = new Request("http://localhost/transactions", {
+    headers: { cookie: cookieString },
+  });
+
+  const user = await getCurrentUserFromRequest(req);
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return <TransactionsClient />;
 }

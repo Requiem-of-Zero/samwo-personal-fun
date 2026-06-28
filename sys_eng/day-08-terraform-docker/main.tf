75fe2468628335f2d8945233e662a5ece56894c0
requiem-of-zero <theesamwong@gmail.com>
2026-06-28 00:18:43 -0700

    (terraform) Update platform structure to use template files for host inventory and hosts file.

--- sys_eng/day-08-terraform-docker/main.tf
+++ sys_eng/day-08-terraform-docker/main.tf
@@ -14,21 +14,33 @@ terraform {
 provider "docker" {}
 
 resource "local_file" "host_inventory" {
-  filename = "${path.module}/hosts/inventory.txt"
+  filename = "${path.module}/hosts/inventory.yml"
 
-  content = <<EOT
-Monitor:
-  Name: ${var.monitor_name}
-  Role: monitoring
-  Host: ${var.monitor_host}
-  Domain: ${var.monitor_domain}
+  file_permission      = "0644"
+  directory_permission = "0755"
 
-Restaurant:
-  Name: ${var.restaurant_name}
-  Role: restaurant-pos
-  Host: ${var.restaurant_host}
-  Domain: ${var.restaurant_domain}
-EOT
+  content = templatefile("${path.module}/templates/inventory.yml.tftpl", {
+    monitor_name      = var.monitor_name
+    monitor_host      = var.monitor_host
+    monitor_domain    = var.monitor_domain
+    restaurant_name   = var.restaurant_name
+    restaurant_host   = var.restaurant_host
+    restaurant_domain = var.restaurant_domain
+  })
+}
+
+resource "local_file" "hosts_file" {
+  filename = "${path.module}/hosts/hosts"
+
+  file_permission      = "0644"
+  directory_permission = "0755"
+
+  content = templatefile("${path.module}/templates/hosts.tftpl", {
+    monitor_host      = var.monitor_host
+    monitor_domain    = var.monitor_domain
+    restaurant_host   = var.restaurant_host
+    restaurant_domain = var.restaurant_domain
+  })
 }
 
 output "platform_summary" {

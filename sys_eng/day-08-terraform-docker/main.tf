2c19e4fff9cd0623b7cf3d8aba37be6bff055f43
requiem-of-zero <theesamwong@gmail.com>
2026-06-28 13:01:35 -0700

    (terraform) Generated docker compose file with terraform template file for a restaurant

--- sys_eng/day-08-terraform-docker/main.tf
+++ sys_eng/day-08-terraform-docker/main.tf
@@ -37,6 +37,20 @@ resource "local_file" "hosts_file" {
   })
 }
 
+resource "local_file" "restaurant_compose" {
+  for_each = var.restaurants
+
+  filename = "${path.module}/generated/restaurants/${each.key}/docker-compose.yml"
+
+  file_permission      = "0644"
+  directory_permission = "0755"
+
+  content = templatefile("${path.module}/templates/restaurant-compose.yml.tftpl", {
+    restaurant_key = each.key
+    restaurant     = each.value
+  })
+}
+
 output "platform_summary" {
   value = {
     monitors    = var.monitors

b80a2619e2970f36ecc5a3f59b03893e6a798cac
requiem-of-zero <theesamwong@gmail.com>
2026-06-27 23:29:27 -0700

    (terraform) Add pos hosts and monitoring hosts to infrastructure. Automating populating hosts to an inventories file.

--- sys_eng/day-08-terraform-docker/main.tf
+++ sys_eng/day-08-terraform-docker/main.tf
@@ -4,15 +4,47 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
   }
 }
 
 provider "docker" {}
 
-output "restaurant_summary" {
+resource "local_file" "host_inventory" {
+  filename = "${path.module}/hosts/inventory.txt"
+
+  content = <<EOT
+Monitor:
+  Name: ${var.monitor_name}
+  Role: monitoring
+  Host: ${var.monitor_host}
+  Domain: ${var.monitor_domain}
+
+Restaurant:
+  Name: ${var.restaurant_name}
+  Role: restaurant-pos
+  Host: ${var.restaurant_host}
+  Domain: ${var.restaurant_domain}
+EOT
+}
+
+output "platform_summary" {
   value = {
-    name   = var.restaurant_name
-    domain = var.restaurant_domain
-    host   = var.restaurant_host
+    monitor = {
+      role   = "monitoring"
+      name   = var.monitor_name
+      domain = var.monitor_domain
+      host   = var.monitor_host
+    }
+
+    restaurant = {
+      role   = "restaurant-pos"
+      name   = var.restaurant_name
+      domain = var.restaurant_domain
+      host   = var.restaurant_host
+    }
   }
 }
--- sys_eng/day-08-terraform-docker/variables.tf
+++ sys_eng/day-08-terraform-docker/variables.tf
@@ -13,6 +13,23 @@ variable "restaurant_domain" {
 variable "restaurant_host" {
   description = "IP address or hostname of the Ubuntu machine"
   type        = string
-  default     = "192.168.1.21"
+  default     = "192.168.1.28"
 }
 
+variable "monitor_name" {
+  description = "Name for central monitoring host"
+  type        = string
+  default     = "central-monitor"
+}
+
+variable "monitor_domain" {
+  description = "DNS name that will route traffic to the monitoring server"
+  type        = string
+  default     = "monitor.samuelwong.xyz"
+}
+
+variable "monitor_host" {
+  description = "IP address of hostname of the Ubuntu machine running Prometheus and Grafana"
+  type        = string
+  default     = "192.168.1.58"
+}
